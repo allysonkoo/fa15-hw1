164aa1ca4fd3f9ef0bc9b447cac5a65e7a712031
Ally Koo <allysonkoo@berkeley.edu>
2015-09-17 14:34:33 -0700

    first homework

--- foobar.rb
+++ foobar.rb
@@ -1,3 +1,13 @@
 class Foobar
-  # Q4 CODE HERE
+
+   def Foobar.baz(a)
+  	a.map! { |item| item.to_i}
+  	a.map! { |item| item + 2}
+  	a.delete_if { |item| item % 2 != 0}
+  	a = a.uniq
+  	a.delete_if { |item| item > 10}
+  	return a.inject{ |sum, item| sum + item}
+
+  end
+
 end
